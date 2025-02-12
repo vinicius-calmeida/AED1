5a2ebdb6e5801b1a9961de615baae49332e57610
vinicius-calmeida <vinicius.constantino@unifesp.br>
2025-02-12 18:38:21 -0300

    1007.c

---
+++ 1007.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+
+    int A, B, C, D, dif;
+    scanf("%d\n%d\n%d\n%d", &A, &B, &C, &D);
+    dif = (A * B - C * D);
+    printf("DIFERENCA = %d\n", dif);
+
+    return 0;
+}
