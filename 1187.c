2317bc5598010684bb73ed1e41dfcc6c5a89f083
vinicius-calmeida <vinicius.constantino@unifesp.br>
2025-02-12 11:22:39 -0300

    Create 1187.c

---
+++ 1187.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+int main() {
+    char O;
+    double M[12][12];
+    double resultado = 0.0;
+    int contador = 0;
+
+    scanf(" %c", &O);
+
+
+    for (int i = 0; i < 12; i++) {
+        for (int j = 0; j < 12; j++) {
+            scanf("%lf", &M[i][j]);
+        }
+    }
+
+
+    for (int i = 0; i < 6; i++) {
+        for (int j = i + 1; j < 12 - i - 1; j++) {
+            resultado += M[i][j];
+            contador++;
+        }
+    }
+
+
+    if (O == 'M') {
+        resultado /= contador;
+    }
+
+ 
+    printf("%.1f\n", resultado);
+
+    return 0;
+}
